5e6f6b130a330154e2e4abe7056ed90800c15d32
krzkamgit@wp.pl <krzkamgit@wp.pl>
2021-05-25 16:24:30 +0200

    Defining the Email Department

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,7 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class Email {
     private String firstName;
     private String lastName;
@@ -12,9 +14,22 @@ public class Email {
     public Email(String firstName, String lastName){
         this.firstName = firstName;
         this.lastName = lastName;
-        System.out.println("EMAIL CREATED: "+this.firstName+" "+this.lastName);
+
+
+        //Call a method asking for the department - return department
+        this.department = setDepartment();
+        System.out.println("EMAIL CREATED: "+this.firstName+" "+this.lastName+" "+this.department);
     }
     //Ask for the department
+    private String setDepartment(){
+        System.out.print("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter the department code: \n");
+        Scanner in = new Scanner(System.in);
+        int depChoice = in.nextInt();
+        if (depChoice == 1) return "sales";
+        else if (depChoice ==2 ) return "dev";
+        else if (depChoice ==3) return "acct";
+        else return "";
+    }
 
     //Generate a random password
 
