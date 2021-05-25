fb60c00c825f9d65658e6d408e0f000323d111f6
krzkamgit@wp.pl <krzkamgit@wp.pl>
2021-05-25 16:38:15 +0200

    Generating a Random Password

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -8,6 +8,7 @@ public class Email {
     private String password;
     private String department;
     private int mailboxCapacity;
+    private int defaultsPasswordLenght = 10;
     private String alternateEmail;
 
     //Constructor to receive the first name and last name
@@ -18,7 +19,11 @@ public class Email {
 
         //Call a method asking for the department - return department
         this.department = setDepartment();
+        //Call a name that returns random password
+        this.password=randomPassword(defaultsPasswordLenght);
         System.out.println("EMAIL CREATED: "+this.firstName+" "+this.lastName+" "+this.department);
+        System.out.println("Your Password is: "+this.password);
+
     }
     //Ask for the department
     private String setDepartment(){
@@ -32,7 +37,15 @@ public class Email {
     }
 
     //Generate a random password
-
+    private String randomPassword(int length){
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
+        char[]password = new char[length];
+        for (int i=0;i<length;i++){
+            int rand = (int)(Math.random()*passwordSet.length());
+            password[i]=passwordSet.charAt(rand);
+        }
+        return new String(password);
+    }
     //Set the mailbox capacity
 
     //Set the alternate email
