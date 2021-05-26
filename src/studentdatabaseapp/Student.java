ac8ac791787bdd43288c3c2d2a1eb1a97853b2d4
krzkamgit@wp.pl <krzkamgit@wp.pl>
2021-05-26 11:34:31 +0200

    Creating the Unique Student ID

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -6,10 +6,11 @@ public class Student {
     private String firstName;
     private String lastName;
     private int gradeYear;
-    private int studentID;
+    private String studentID;
     private String courses;
     private String tuitionBalance;
-    private int costOfCourse = 600;
+    private static int costOfCourse = 600;
+    private static int id = 1000;
 
     //Constructor: prompt user to enter student's name and year
     public Student(){
@@ -22,9 +23,17 @@ public class Student {
 
         System.out.print("1 - Freshmen\n2- Sophmore\n3 - Junior\n4 - Senior\nEnter student class level: ");
         this.gradeYear=in.nextInt();
-        System.out.println(firstName+" "+lastName+" "+gradeYear);
+
+
+        setStudentID();
+        System.out.println(firstName+" "+lastName+" "+gradeYear+" "+studentID);
     }
     //Generate an ID
+    private void setStudentID(){
+        //Grade level + id
+        id++;
+        this.studentID = gradeYear+""+id;
+    }
 
     //Enroll in courses
 
