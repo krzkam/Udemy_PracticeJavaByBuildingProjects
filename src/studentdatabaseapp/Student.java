b0a6e6fe7bda57c8d34fed7b28857bbb45ba3ab5
krzkamgit@wp.pl <krzkamgit@wp.pl>
2021-05-26 10:20:50 +0200

    Creating the Outline for Student Class

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -1,4 +1,23 @@
 package studentdatabaseapp;
 
 public class Student {
+    private String firstName;
+    private String lastName;
+    private String gradeYear;
+    private int studentID;
+    private String courses;
+    private String tuitionBalance;
+    private int costOfCourse = 600;
+
+    //Constructor: prompt user to enter student's name and year
+
+    //Generate an ID
+
+    //Enroll in courses
+
+    //View balance
+
+    //Pay Tuition
+
+    //Show status
 }
