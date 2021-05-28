aef75885ef312fc619a1f6b2355ae51209c2782c
krzkamgit@wp.pl <krzkamgit@wp.pl>
2021-05-28 12:23:39 +0200

    Writing a CSV Reader Utility

--- src/bankaccountapp/BankAccountApp.java
+++ src/bankaccountapp/BankAccountApp.java
@@ -1,7 +1,10 @@
 package bankaccountapp;
 
+import java.util.List;
+
 public class BankAccountApp {
     public static void main(String[] args){
+        /*
         Checking chkacc1 = new Checking("Krz Kam","123456789",1500);
         Savings savacc1 = new Savings("Richy Rich","123456782",2500);
 
@@ -9,12 +12,20 @@ public class BankAccountApp {
 
         savacc1.showInfo();
         chkacc1.showInfo();
-
+        */
 
 
 
         //Read a CSV File then create new accounts based on that data
-
+        String file = "C:\\Users\\KAMINKR1\\Downloads\\NewBankAccounts.csv";
+        List<String[]> newAccountHolders = utilities.CSV.read(file);
+        for (String[] newAccountHolder : newAccountHolders ){
+            System.out.println("NEW ACCOUNT");
+            System.out.println(newAccountHolder[0]);
+            System.out.println(newAccountHolder[1]);
+            System.out.println(newAccountHolder[2]);
+            System.out.println(newAccountHolder[3]);
+        }
 
     }
 }
