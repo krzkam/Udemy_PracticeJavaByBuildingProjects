cb4d053473dba5df033b8dd765ad64ddc1b3b0c0
krzkamgit@wp.pl <krzkamgit@wp.pl>
2021-05-28 11:20:19 +0200

    Implementing the Savings Account Safety Deposit Box

--- src/bankaccountapp/Savings.java
+++ src/bankaccountapp/Savings.java
@@ -2,17 +2,26 @@ package bankaccountapp;
 
 public class Savings extends Account{
     //List properties specific to the Savings account
-    int safetyDepistBoxID;
-    int safetyDepisitBoxKey;
+    int safetyDepostBoxID;
+    int safetyDepositBoxKey;
 
     //Constructor to initialize settings for the Saving properties
     public Savings(String name, String ssn, double initDeposit) {
         super(name,ssn,initDeposit);
         accountNumber = "1"+accountNumber;
+        setSafetyDepositBox();
+    }
+    private void setSafetyDepositBox(){
+        safetyDepostBoxID =(int)(Math.random()*Math.pow(10,3));
+        safetyDepositBoxKey=(int)(Math.random()*Math.pow(10,4));
     }
     //List any methods specific to savings account
     public void showInfo(){
         super.showInfo();
-        System.out.println("ACCOUNT TYPE: Savings");
+        System.out.println(
+                " Your Savings Account Features"+
+                        "\n Safety Deposit Box ID "+safetyDepostBoxID+
+                        "\n Safety Deposit Key "+safetyDepositBoxKey
+        );
     }
 }
