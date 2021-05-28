d9aaed72ef940ed1036ab8442766b97300e4920c
krzkamgit@wp.pl <krzkamgit@wp.pl>
2021-05-28 10:34:11 +0200

    Generating the Account Number

--- src/bankaccountapp/Account.java
+++ src/bankaccountapp/Account.java
@@ -7,13 +7,26 @@ public abstract class Account implements IBaseRate{
     String accountNumber;
     double balance;
     double rate;
+    static int index = 10000;
 
     //Constructor to set base properties and initialize the account
     public Account(String name, String ssn, double initDeposit){
         this.name=name;
         this.ssn=ssn;
-        balance=initDeposit;
-        System.out.println("NAME: "+name+"SSN: "+ssn+"BALANCE: $"+balance);
+        this.balance=initDeposit;
+        System.out.println("NAME: "+name+" SSN: "+ssn+" BALANCE: $"+balance);
+
+        //Set account number
+        index++;
+        this.accountNumber=setAccountNumber();
+        System.out.println("ACCOUNT NUMBER "+this.accountNumber);
+    }
+
+    private String setAccountNumber(){
+        String lastTwoOfSSN = ssn.substring(ssn.length()-2,ssn.length());
+        int uniqueID = index;
+        int randomNumber = (int)(Math.random()*Math.pow(10,3));
+        return lastTwoOfSSN+uniqueID+randomNumber;
     }
     //List common methods
 }
