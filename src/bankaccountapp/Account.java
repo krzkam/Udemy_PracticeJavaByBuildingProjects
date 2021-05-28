75481fef7f8675b6c3ce84a95b542eb540bcd1dc
krzkamgit@wp.pl <krzkamgit@wp.pl>
2021-05-28 11:53:14 +0200

    Implementing the Compound Interest Rate Method

--- src/bankaccountapp/Account.java
+++ src/bankaccountapp/Account.java
@@ -30,6 +30,12 @@ public abstract class Account implements IBaseRate{
         return lastTwoOfSSN+uniqueID+randomNumber;
     }
 
+    public void compound(){
+        double accruedInterest = balance*(rate/100);
+        balance=balance+accruedInterest;
+        System.out.println("Accrued Interest $"+accruedInterest);
+        printBalance();
+    }
 
     //List common methods - transactions
     public void deposit(double amount){
--- src/bankaccountapp/BankAccountApp.java
+++ src/bankaccountapp/BankAccountApp.java
@@ -5,12 +5,14 @@ public class BankAccountApp {
         Checking chkacc1 = new Checking("Krz Kam","123456789",1500);
         Savings savacc1 = new Savings("Richy Rich","123456782",2500);
 
+        savacc1.compound();
+
         savacc1.showInfo();
         chkacc1.showInfo();
 
-        savacc1.deposit(5000);
-        savacc1.withdraw(200);
-        savacc1.transfer("Brokerage",3000);
+
+
+
         //Read a CSV File then create new accounts based on that data
 
 
